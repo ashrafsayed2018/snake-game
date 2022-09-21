659acc76e3af5c017a59f1b3d7dcf1310739fb8d
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-09-21 17:47:38 +0300

    codeing the actual snake

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:snake_game/widget/blank_pixel.dart';
+import 'package:snake_game/widget/food_pixel.dart';
+import 'package:snake_game/widget/snake_pixel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +15,11 @@ class _HomePageState extends State<HomePage> {
 
   int rowSize = 10;
   int totalNumberOfSquares = 100;
+
+  // snak position
+  List<int> snakePostion = [0, 1, 2];
+  // food postion
+  int foodPosition = 50;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +42,11 @@ class _HomePageState extends State<HomePage> {
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: totalNumberOfSquares,
                   itemBuilder: (BuildContext context, int index) {
+                    if (snakePostion.contains(index)) {
+                      return const SnakePixel();
+                    } else if (foodPosition == index) {
+                      return const FoodPixel();
+                    }
                     return const BlankPixel();
                   },
                 ),
@@ -42,7 +54,15 @@ class _HomePageState extends State<HomePage> {
             ),
             // play button
             Expanded(
-              child: Container(),
+              child: Container(
+                child: Center(
+                  child: MaterialButton(
+                    color: Colors.pink,
+                    onPressed: () {},
+                    child: const Text("Play"),
+                  ),
+                ),
+              ),
             )
           ],
         ));
